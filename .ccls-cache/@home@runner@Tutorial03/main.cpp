7aa00e6238f2c1325d3054f57a7f1ba323c624a0
it22198990 <127378135+it22198990@users.noreply.github.com>
2023-03-23 06:14:19 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Tutorial03/main.cpp
@@ -0,0 +1,77 @@
+/*
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
+  char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
+
+  cout << setw(5) << "No" << setw(15) << "Name" << setw(10) << "Marks" << endl;
+
+  for (int r = 0; r < 5; r++) {
+    cout << setw(5) << setiosflags(ios::fixed) << r + 1 << setw(15)
+         << setiosflags(ios::fixed) << names[r] << setw(10)
+         << setiosflags(ios::fixed) << setprecision(2) << marks[r] << endl;
+  }
+
+  return 0;
+}
+*/
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int volume(int height, int width, int length);
+
+int main() {
+  int box1Height, box1Width, box1Length;
+  int box2Height, box2Width, box2Length;
+  int totalVolume, totalSurface;
+
+  cout << "Enter Box 1 Height : ";
+  cin >> box1Height;
+  cout << "Enter Box 1 Width : ";
+  cin >> box1Width;
+  cout << "Enter Box 1 Length : ";
+  cin >> box1Length;
+
+  cout << "Enter Box 2 Height : ";
+  cin >> box2Height;
+  cout << "Enter Box 2 Width : ";
+  cin >> box2Width;
+  cout << "Enter Box 2 Length : ";
+  cin >> box2Length;
+
+  totalVolume = volume(box1Height, box1Width, box1Length) +
+                volume(box2Height, box2Width, box2Length);
+
+  cout << "Volume of Box is " << totalVolume << endl;
+
+  return 0;
+}
+
+int volume(int height, int width, int length) {
+  return height * width * length;
+}
+*/
+
+#include <iostream>
+
+using namespace std;
+
+struct Box {
+  int height;
+  int width;
+  int length;
+};
+
+int main() {
+  struct Box box1;
+  struct Box box2;
+
+  return 0;
+}
